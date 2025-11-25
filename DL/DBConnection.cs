8485da486640fb2de12640f46a890535043e5c41
Husnain <husnainhere.2019.5@gmail.com>
2025-11-25 22:42:14 +0500

    feat(database): add full database schema, relations, and configuration

---
+++ DL/DBConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chhipa_Motors.DL
+{
+    public class DBConnection
+    {
+        private SqlConnection con;
+
+        public DBConnection()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["ChhipaMotorsDB"].ConnectionString;
+
+            con = new SqlConnection(conString);
+        }
+    }
+}
--- obj/Debug/net8.0-windows/Chhipa Motors.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Chhipa Motors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chhipa Motors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51da2ca6908ad653b6f157b80c9c56155654c9eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80590177c00cac16dc43508efee20b8e5dcf17b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chhipa Motors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chhipa Motors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
